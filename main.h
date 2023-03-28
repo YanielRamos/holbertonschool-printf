d16d38ffb621f7443f1de31c3119f327cc67f03e
YanielRamos <yanielramos0504@gmail.com>
2023-03-28 21:14:17 +0000

    primer prototipo de la funcion printf con los conversion specifiers, c,s, & %

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef main_h
+#define main_h
+
+/* libraries */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+/* prototypes */
+int _printf(const char *format, ...);
+
+#endif
